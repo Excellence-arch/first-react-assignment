406bcd319944c5d527fa673ddbcf65f70b6ab1e9
excellence-arch <oladipupomichael9@gmail.com>
2021-07-14 09:55:38 +0100

    I changed the cards into components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
+import SearchLogo from './icons8_Search_32.png'
+import LockLogo from './icons8_Lock_2.ico'
 // import Words from './components/Words'
 import './App.css';
 
@@ -13,7 +15,11 @@ class App extends React.Component {
           <Header/>
         {/* </header> */}
         <main>
-          <Card/>
+          <section className='cardGroup'>
+          <Card clip={"polygon(0% 0%, 100% 0%, 100% 100%, 95% 88%, 0% 88%)"} bgColor={"rgba(253, 189, 159, 0.7)"} logo={SearchLogo} margin="150px" text="Find the best answer to your technical question, help others answer theirs" btnColor="rgb(204, 92, 40)" join="Join the Community" link="search content"/>
+
+          <Card clip={"polygon(0% 0%, 100% 0%, 100% 85%, 5% 86%, 0 100%)"} bgColor={"rgba(207,234,254, 1)"} logo={LockLogo} margin="30px" text="Want a secure, private space for your technical knowledge?" btnColor="rgb(0,119,200)" join="Create a free team" link="search team"/>
+          </section>
           <p style={{fontSize:45, color: "white", position: "absolute", marginLeft: 380, textAlign: "center"}}>Every <span style={{color: "orange"}}>developer</span> has a <br/> tab open to Stack Overflow</p>
         </main>
       </section>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import SearchLogo from './icons8_Search_32.png'
-import LockLogo from './icons8_Lock_2.ico'
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <section className="cardHeader">
-                <div className="card1">
-                    <img id='card1Image' src={SearchLogo} />
-                    <p className="card1Text">Find the best answer to your technical question, help others answer theirs</p> <br/>
-                    <button className="card1Btn">Join the Community</button><br/>
-                    <span>or </span> <a className="card1Link" href="">search content</a>
+            // <section className="cardHeader">
+                <div className="card" style={{clipPath : this.props.clip, width: 500, backgroundColor: this.props.bgColor, marginLeft: this.props.margin}}>
+                    <img id='cardImage' src={this.props.logo} alt={this.props.logoName} />
+                    <p className="card1Text">{this.props.text}</p> <br/>
+                    <button className="card1Btn" style={{backgroundColor: this.props.btnColor }}>{this.props.join}</button><br/>
+                    <span>or </span> <a className="card1Link" href="#">{this.props.link}</a>
                 </div>
 
-                <div className="card2">
+                /* <div className="card2">
                     <img id='card2Image' src={LockLogo} />
                     <p className="card2Text">Want a secure, private space for your technical knowledge?</p> <br/>
                     <button className="card2Btn">Create a free team</button><br/>
                     <span>or </span> <a className="card1Link" href="">discover teams</a>
-                </div>
-            </section>
+                </div> 
+             </section> */
         );
     };
 }
